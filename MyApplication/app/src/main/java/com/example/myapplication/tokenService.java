0fff1f857cbd7e6cc22ed0c519d0c6d8d8019ae3
karenMora <karen.mora@mail.escuelaing.edu.co>
2019-12-09 18:25:38 -0500

    .

--- MyApplication/app/src/main/java/com/example/myapplication/tokenService.java
+++ MyApplication/app/src/main/java/com/example/myapplication/tokenService.java
@@ -1,5 +1,10 @@
 package com.example.myapplication;
 
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
 public interface  tokenService {
     @POST("/auth")
     Call<Token> login(@Body User login);
